898ba6ec697613cb3fe3982b1b2f1496f3a58147
robot <viewmuse@gmail.com>
2023-03-22 07:29:23 +0800

    fix jwt

--- app/config/auth.py
+++ app/config/auth.py
@@ -1,11 +1,11 @@
 from fastapi import Depends, HTTPException
-from fastapi.security import OAuth2PasswordBearer
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from app.config.settings import get_settings
 from app.database.connection import get_db_client
 from motor.motor_asyncio import AsyncIOMotorClient
 from fastapi_jwt_auth import AuthJWT as BaseAuthJWT
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
+http_bearer = HTTPBearer()
 
 settings = get_settings()
 
@@ -14,15 +14,23 @@ class CustomAuthJWT(BaseAuthJWT):
     def _get_secret_key(self, algorithm, action):
         return settings.AUTHJWT_SECRET_KEY
 
+    # 使用 decode_jwt 方法
+    def decode_token(self, token, verify_exp=True, csrf_token=None):
+        return self.decode_jwt(token, secret=settings.AUTHJWT_SECRET_KEY, verify_exp=verify_exp, csrf_token=csrf_token)
+
 
 def create_custom_jwt_auth():
     return CustomAuthJWT()
 
 
-async def get_current_user(token: str = Depends(oauth2_scheme)) -> str:
+async def get_current_user(credentials: HTTPAuthorizationCredentials = Depends(http_bearer)) -> str:
+    token = credentials.credentials
+    print(f"Token: {token}")  # 输出接收到的 token
     try:
         jwt = create_custom_jwt_auth()
         payload = jwt.decode_token(token)
+        print(f"Payload: {payload}")  # 输出解码后的 payload
         return payload["sub"]
     except Exception as e:
+        print(f"Error: {e}")  # 输出错误信息
         raise HTTPException(status_code=401, detail="Invalid JWT token")
--- app/config/settings.py
+++ app/config/settings.py
@@ -4,11 +4,11 @@ from pydantic import BaseSettings
 class Settings(BaseSettings):
     MONGODB_URL: str = "mongodb://localhost:27017/"
     MONGODB_DATABASE_NAME: str = "hmconsulting"
-    AUTHJWT_SECRET_KEY = "your_secret_key_here"
+    AUTHJWT_SECRET_KEY: str = "your_secret_key_here"
 
     class Config:
         env_file = ".env"
 
 
-def get_settings():
+def get_settings() -> Settings:
     return Settings()
